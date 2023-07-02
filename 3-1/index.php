35634da14e9d36372807051afad7ded63b0d9bff
Lulu-K <diamondgirldx@yahoo.co.jp>
2023-07-02 22:35:55 +0900

    Create index.php

---
+++ 3-1/index.php
@@ -0,0 +1,25 @@
+<?php
+$num = 1;
+for($i=1;$i<101;$i++){
+
+if ($i % 3 ==0 && $i % 5 ==0){
+    echo "FizzBuzz!!";
+}
+
+elseif ($i % 3 ==0){
+    echo "Fizz!";
+}
+
+elseif ($i % 5 ==0){
+    echo "Buzz!";
+}
+
+else {
+    echo $i;
+}
+
+echo "<br>";
+
+}
+
+?>
